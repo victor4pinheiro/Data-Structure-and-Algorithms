06d55eabb0d681687dbc3d5dd6a421b4ce684595
victor4pinheiro <vmcpinheiro@yahoo.com.br>
2020-05-04 13:30:58 -0300

    Condition switch..case with struc

---
+++ conditionalStructure.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+typedef struct {
+  int age;
+} AgeMajority;
+
+int main() {
+
+  AgeMajority ageConditionally;
+  
+  printf("How old are you?");
+  scanf("%i", &ageConditionally.age);
+
+  if (ageConditionally.age < 3 ) {
+    printf("Baby");
+  } else if (ageConditionally.age < 14) {
+    printf("Child");
+  } else if (ageConditionally.age < 18) {
+    printf("Teenager");
+  } else if (ageConditionally.age < 65) {
+    printf("Adult");
+  } else {
+    printf("Elderly");
+  }
+  return 0;
+        
+}
+
